2d3da4b45d719ca0ed7f1bd9296817e9dc753d0e
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-07-31 19:12:37 +0530

    program12.java

--- java fundamentals/Flow_control_statements/program12.java
+++ java fundamentals/Flow_control_statements/program12.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class PrimeSimple {
+public class program12{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
