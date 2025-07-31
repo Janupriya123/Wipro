2c55fb338568847d6e0859945199b4fd1250d570
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-07-31 19:20:21 +0530

    program15.java

---
+++ java fundamentals/Flow_control_statements/program15.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class program15{
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int r=0;
+        while (n!=0) 
+        {
+            int d=n%10;       
+            r=r*10+d;      
+            n=num/10;            
+        }
+        sc.close();
+    }
+}
