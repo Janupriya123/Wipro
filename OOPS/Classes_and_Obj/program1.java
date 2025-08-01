32293e7982df8a6754caf3e9c9bfbd491a46f7e5
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-08-01 19:35:59 +0530

    program1.java

--- OOPS/Classes_and_Obj/program1.java
+++ OOPS/Classes_and_Obj/program1.java
@@ -3,29 +3,17 @@ class Box {
     double w, h,b;
     Box(double width, double height, double breadth) 
   {
-        this.w=width;
-        this.h=height;
-        this.b=breadth;
+         w=width;
+         h=height;
+         b=breadth;
     }
    public static double volume() 
   {
-        return width*height*breadth;
+        return  w*h*b;
     }
 }
 
-class Calculator 
-{
-     public static int powerInt(int num1, int num2)
-    {
-        return (int) Math.pow(num1, num2);
-    }
-     public static double powerDouble(double num1, int num2) 
-    {
-        return Math.pow(num1, num2);
-    }
-}
-
-public class Main
+ public class Main
   {
     public static void main(String[] args)
   {
@@ -34,12 +22,7 @@ public class Main
         double h=sc.nextDouble();
         double b=sc.nextDouble();
         Box box=new Box(w,h,b);
-        int ibase=sc.nextInt();
-        int iexp=sc.nextInt();
-        System.out.println("Result of powerInt " + Calculator.powerInt(ibase,iexp));
-        double dbase=sc.nextDouble();
-        int  dexp=sc.nextInt();
-        System.out.println("Result of powerDouble " + Calculator.powerDouble(dbase,dexp));
+        System.out.println(box.volume());
         sc.close();
    }
 }
