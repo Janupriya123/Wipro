6c209f39d488bc69ea2a677abbef548f0ac2feef
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-08-01 10:36:04 +0530

    program8.java

---
+++ java fundamentals/Array/program8.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class program8
+{
+    public static void main(String[] args)
+  {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+       for(int i=0;i<n;i++) 
+       {
+            arr[i]=sc.nextInt();
+        }
+       int sum=0;
+       boolean skip=false;
+       for(int num:arr)
+         {
+            if(num==6) {
+                skip=true;
+                continue;
+            }
+            if (num==7&&skip) 
+            {
+                skip=false;
+                continue;
+            }
+            if(!skip) 
+            {
+                sum +=num;
+            }
+        }
+     System.out.println("Sum"+sum);
+     sc.clsoe();
+    }
+}
