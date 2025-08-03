595296446c4d4ac0134aee8a250d4b163b7300bd
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-08-03 21:46:00 +0530

    program2_Package2.java

---
+++ Abstraction_Packages_ExceptionHandling/Packages/program2/program2_Package2.java
@@ -0,0 +1,9 @@
+import com.wipro.automobile.ship.Compartment;
+public class TestShipCompartment
+  {
+    public static void main(String[] args)
+    {
+        Compartment c = new Compartment(10.5, 8.2, 6.3);
+        c.displayDimensions();
+    }
+}
